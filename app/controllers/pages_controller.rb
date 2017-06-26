2c451e513af1ce0ece19caae073cb09016d11170
Andrew Carlson <carlsonaf@g.cofc.edu>
2017-06-26 12:24:45 +0000

    Finishing Task 1

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,15 @@
 class PagesController < ApplicationController
-  def home
-  end
+  def send_message
+    account_sid = "ACa41f31fd1164c79a6fb46bcf1e3fb438"
+    auth_token = "c44a56784f930da27130160095b87ae6"
 
-  def sms
-    raise "Action sms in the UsersController is not implemented! received params #{params.to_json}"
+        @client = Twilio::REST::Client.new account_sid, auth_token
+        @twilio_number = "+7815145857" 
+        message = @client.messages.create(
+          :from => @twilio_number,
+          :to => 7814003837,
+          :body => alert_message
+        )
+        puts message.to
   end
 end
